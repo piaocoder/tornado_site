916e6c62a7d0d5a6af2a7ef6e092c7e0c897cb5e
exbot <331837926@q q>
2016-10-14 02:58:18 +0800

    UI更新，同时准备新增一个查询项目

--- acmCralwer/view.py
+++ acmCralwer/view.py
@@ -4,4 +4,7 @@ import tornado.web
 
 class queryHandler(tornado.web.RequestHandler):
     def get(self, *args, **kwargs):
-        self.render('queryProblem/index.html')
+        self.render('queryProblem/index.html',error=False)
+
+    def post(self, *args, **kwargs):
+        self.render('queryProblem/query.html')
